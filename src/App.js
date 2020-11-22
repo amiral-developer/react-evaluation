6cee09cc4dd294f53e55407e1b3f97f2fe50c451
Amir BEN SLIMANE <amir.ben-slimane-ext@treezor.com>
2020-11-22 03:36:06 +0100

    Display posts and comments

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const API_URL = "https://jsonplaceholder.typicode.com";
 
 function App() {
+  const [posts, setPosts] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const postsResponse = await fetch(`${API_URL}/posts`);
+
+      if (postsResponse.ok) {
+        const posts = await postsResponse.json();
+
+        if (posts) {
+          const postsWithComments = await Promise.all(
+            posts.map(async (post) => {
+              if (post.id) {
+                const commentsResponse = await fetch(
+                  `${API_URL}/comments?postId=${post.id}`
+                );
+
+                if (commentsResponse.ok) {
+                  const comments = await commentsResponse.json();
+
+                  return {
+                    ...post,
+                    comments,
+                  };
+                }
+
+                return post;
+              }
+
+              return post;
+            })
+          );
+
+          setPosts(postsWithComments);
+          console.log(postsWithComments);
+        }
+      }
+    })();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!posts && <p>No posts</p>}
+      {posts &&
+        posts.map((post) => (
+          <div>
+            <p>
+              <strong>{post.title}</strong>
+            </p>
+            <p>{post.body}</p>
+            <ul>
+              {post.comments &&
+                post.comments.length &&
+                post.comments.map((comment) => (
+                  <li>
+                    {comment.email} : {comment.body}
+                  </li>
+                ))}
+            </ul>
+          </div>
+        ))}
     </div>
   );
 }
